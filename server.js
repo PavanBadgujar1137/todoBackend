cc78386d947580e3850933c21fbe9b33b3c52e02
Pavan Amrut Badgujar <pavanabad01@gmail.com>
2024-01-29 02:49:59 +0530

    add cores in here

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors"); // Import the cors middleware
+const cors = require("cors"); // Import the cors middleware
 
 require("dotenv").config();
 app.use(express.json());
+// Define your CORS options
+const corsOptions = {
+  origin: "http://127.0.0.1:5501", // Replace with your frontend domain
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 4000;
 
