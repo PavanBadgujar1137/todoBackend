fbcd4500356972e016c2b539cbd024f8f99d7073
Pavan Amrut Badgujar <pavanabad01@gmail.com>
2024-01-29 06:39:12 +0530

    404 validate

--- controllers/createTodo.js
+++ controllers/createTodo.js
@@ -4,20 +4,32 @@ exports.createTodo = async (req, res) => {
   try {
     const { addTodo } = req.body;
 
+    // Validate if 'addTodo' is provided in the request body
+    if (!addTodo) {
+      return res.status(400).json({
+        success: false,
+        error: "Bad Request",
+        message: "'addTodo' is required in the request body.",
+      });
+    }
+
+    // Create a new todo
     const todo = await Todo.create({ addTodo });
 
-    res.status(200).json({
+    // Respond with success and the created todo
+    res.status(201).json({
       success: true,
       data: todo,
-      message: "TODO Created Successfully!!!!!", // Fixed typo: "meassage" to "message"
+      message: "TODO Created Successfully!",
     });
   } catch (error) {
-    console.error(error);
-    console.log(error);
+    console.error("Error creating todo:", error);
+
+    // Respond with an error and the error message
     res.status(500).json({
       success: false,
-      error: error.message, // Fixed typo: "meassage" to "message"
-      message: "Internal Server Issue!!!!!!!", // Moved the message property inside the JSON object
+      error: error.message,
+      message: "Internal Server Issue!",
     });
   }
 };
